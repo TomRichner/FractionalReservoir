584f74bfd17ec32fe45aedbb6f9cf8f8862025a1
Richner <richner.thomas@mayo.edu>
2026-08-08 23:37:32 -0500

    found some different parameters with test_SRNN2_defaults_overconnected.m

--- scripts/tests/test_SRNN2_defaults_overconnected.m
+++ scripts/tests/test_SRNN2_defaults_overconnected.m
@@ -2,13 +2,26 @@
 %
 % Constructs SRNNModel2 with n_a_E=3 (SFA) and n_b_E=1 (STD), all other
 % parameters at defaults (n=300, T_range=[0,50], lya_method='benettin').
+% Activation is overridden to piecewiseSigmoid(x, S_a, S_c).
 
 close all; clear; clc;
 
-rng_seeds = [1 3] + 4;
+rng_seeds = [19 20]+4;
 
 %% Create model
-model = SRNNModel2('tau_d',0.1,'rng_seeds',rng_seeds,'tau_b_E_rel',0.25,'std_zero_floor',false,'c_E', 0.15/3,'S_c', 0.35,'n_a_E', 3, 'n_b_E',1, 'tau_a_E', logspace(log10(0.25), log10(10), 3),  'tau_b_E_rec', [2], 'f', 0.5, 'level_of_chaos', 10, 'T_range', [0 30]);
+model = SRNNModel2('n',300,'indegree',100,'check_connectivity',true,'level_of_chaos', 1,'S_c',0.0,'tau_d',1,'tau_b_E_rec',1,'rng_seeds',rng_seeds,'c_E', 1/3,'n_a_E', 3, 'n_b_E',1, 'f', 2/3, 'T_range', [0 40], ...
+    'ode_solver', @ode_rk4, 'fs', 200);
+
+%% E:I ratio of 2:1 (f=2/3), with per-synapse inhibition doubled to compensate
+% F = 1/sqrt(n*alpha*(2-alpha)) is the RMT normalization factor (Harris 2023);
+% it depends only on n and alpha, so it is unchanged by f.
+F = model.default_val;
+model.mu_E_tilde =  3*F;   % default
+model.mu_I_tilde = -6*F;   % default is -4*F; doubled since there are half as many I neurons
+
+%% Use the piecewise sigmoid instead of the default logistic sigmoid
+model.activation_function            = @(x) SRNNModel2.piecewiseSigmoid(x, model.S_a, model.S_c);
+model.activation_function_derivative = @(x) SRNNModel2.piecewiseSigmoidDerivative(x, model.S_a, model.S_c);
 
 %% Build, run, and plot
 model.build();
