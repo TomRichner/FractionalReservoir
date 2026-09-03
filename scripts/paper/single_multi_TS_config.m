b79b55b02ce00b9eb960e55b8225c141dd8e74ba
TomRichner <richner.thomas@mayo.edu>
2026-09-03 18:59:35 -0500

    single_multi config now fast2

--- scripts/paper/single_multi_TS_config.m
+++ scripts/paper/single_multi_TS_config.m
@@ -62,7 +62,7 @@ function cfg = single_multi_TS_config(opts)
 
 arguments
     opts.preset_name (1,:) char = 'celltype_pairs_Sc0p2_noise0p025_dualStd_3cond'
-    opts.run_mode    (1,:) char = 'fast'
+    opts.run_mode    (1,:) char = 'fast2'
     opts.run_dir     (1,:) char = 'data/single_multi_TS'
     opts.fig_root    (1,:) char = 'figs/single_multi_TS'
     % Off, like paper_config's own default: figures are built and saved but do
